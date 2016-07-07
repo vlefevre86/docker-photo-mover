d2ce1f863710d9763fc1e34e4bd99f097ad95147
vlefevre86 <Vlefevre86@gmail.com>
2016-07-07 18:15:57 +0200

    Update 30_update_Photo-Mover.sh

--- init/30_update_Photo-Mover.sh
+++ init/30_update_Photo-Mover.sh
@@ -5,3 +5,10 @@
 cd /app/photo-mover
 git pull
 npm install
+
+#install exiftool
+wget http://www.sno.phy.queensu.ca/~phil/exiftool/Image-ExifTool-10.21.tar.gz
+tar -xzf Image-ExifTool-10.21.tar.gz
+rm Image-ExifTool-10.21.tar.gz
+cd Image-ExifTool-10.21
+cp -r exiftool lib /usr/local/bin
