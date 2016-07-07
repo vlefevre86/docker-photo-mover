80baaea2d88624a2b7d0588b607b7db11f7b6b0f
vlefevre86 <Vlefevre86@gmail.com>
2016-07-07 11:04:03 +0200

    Update update.sh

--- init/update.sh
+++ init/update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-[[ ! -d /app/photo-mover/.git ]] && git clone https://github.com/vlefevre86/photo-mover.git /app/photo-mover 2> /tasks/log.txt
+[[ ! -d /app/photo-mover/.git ]] && git clone https://github.com/vlefevre86/photo-mover.git /app/photo-mover
 
 cd /app/photo-mover
-git pull 2> /tasks/log.txt
-npm install 2> /tasks/log.txt
+git pull
+npm install
